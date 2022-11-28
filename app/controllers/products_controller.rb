f796e4ce8a43d9895a45d9f543b8220ca472bdfe
dahniaCreates <danika.ds14@gmail.com>
2022-11-28 17:49:54 -0600

    Added advanced search to project

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,12 @@ class ProductsController < ApplicationController
     wildcard_search = "%#{params[:keywords]}%"
     category_search = "%#{params[:category_id]}%"
 
-    @products = Product.where("name LIKE ?", wildcard_search).page(params[:page]) if :keywords.present?
-    # @products = Product.where(category_id: params[:category_id]).page(params[:page]) if category_id.present?
+    if params[:category_id].present? && params[:keywords].present?
+      @products = Product.where(category_id: params[:category_id]).where("name LIKE ?", wildcard_search).page(params[:page])
+    elsif params[:category_id].present?
+      @products = Product.where(category_id: params[:category_id]).page(params[:page])
+    else
+      @products = Product.where("name LIKE ?", wildcard_search).page(params[:page])
+    end
   end
 end
