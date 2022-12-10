72026b6584e12290a0710b38217a94d5d114d46a
dahniaCreates <danika.ds14@gmail.com>
2022-12-10 17:02:53 -0600

    Added tax calculation for checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,8 +2,12 @@ class CheckoutController < ApplicationController
   def create
     @cart_items = session[:cart]
       @line_items_dictionary = []
+      @taxes_dict = []
+      @total_price = 0
+      @taxes = Tax.where(province: current_user.tax.province)
       @cart_items.each do |item|
         @product = Product.find(item)
+        @total_price +=  (@product.price * 100).round()
         @line_items_dictionary << {
           price_data: {
             currency: 'cad',
@@ -16,9 +20,50 @@ class CheckoutController < ApplicationController
           quantity: 1,
         }
       end
+      @taxes.each do |tax|
+        if tax.GST.present?
+          @taxes_dict << {
+            price_data: {
+              currency: 'cad',
+              unit_amount: (@total_price * tax.GST).to_i,
+              product_data: {
+                name: "GST",
+                description: "Goods and Services Tax",
+              },
+            },
+            quantity: 1,
+          }
+          end
+        if tax.PST.present?
+          @taxes_dict << {
+            price_data: {
+              currency: 'cad',
+              unit_amount: (@total_price * tax.PST).to_i,
+              product_data: {
+                name: "PST",
+                description: "Provincial Sales Tax",
+              },
+            },
+            quantity: 1,
+          }
+        end
+        if tax.HST.present?
+          @taxes_dict << {
+            price_data: {
+              currency: 'cad',
+              unit_amount: (@total_price * tax.HST).to_i,
+              product_data: {
+                name: "HST",
+                description: "Harmonized Sales Tax",
+              },
+            },
+            quantity: 1,
+          }
+        end
+      end
     @session = Stripe::Checkout::Session.create({
         payment_method_types: ['card'],
-        line_items: [@line_items_dictionary],
+        line_items: [@line_items_dictionary + @taxes_dict],
         mode: 'payment',
         success_url: root_url,
         cancel_url: root_url,
